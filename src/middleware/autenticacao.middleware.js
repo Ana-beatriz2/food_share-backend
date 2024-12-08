7263805e5bd463d8a9e4b5f9b19e54a7ce30c400
Ana-beatriz2 <caldas.ana9@gmail.com>
2024-12-07 22:01:23 -0300

    feat: adiciona middleware de autenticação

---
+++ src/middleware/autenticacao.middleware.js
@@ -0,0 +1,28 @@
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+function auth(req, res, next){
+    try {
+
+        const authToken = req.headers.authorization;
+    
+        if (!authToken) throw new TokenNaoIdentificado();
+        
+        const parts = authToken.split(" ");
+    
+        if (parts.length !== 2) throw new TokenInvalido();
+    
+        const [, token] = parts;
+    
+        jwt.verify(token, process.env.TOKEN_KEY, (error, data) => {
+            if (error) return res.status(401).json({ message: `Token error - ${error.message}`});
+    
+            req.user_id = { id: data.id, nome: data.nome, email: data.email};
+            return next();
+        })
+    } catch (error) {
+        return res.status(error.status || 401).json({ "message": error.errorMessage || "Houve um erro na autenticação" });
+    }
+}
+
+module.exports = auth;
