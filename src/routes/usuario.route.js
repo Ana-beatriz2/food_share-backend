b60c275e8f7394c197a759bd32b9293d7c6dccad
Ana-beatriz2 <caldas.ana9@gmail.com>
2024-12-08 22:55:45 -0300

    feat: adiciona middleware de autenticação na rote de create usuário

--- src/routes/usuario.route.js
+++ src/routes/usuario.route.js
@@ -2,8 +2,9 @@ const express = require('express');
 const router = express.Router();
 const UsuarioController = require('../controllers/usuario.controller.js');
 const { validaUsuario } = require("../dto/usuario.dto.js");
+const { auth } = require("../middleware/autenticacao.middleware.js");
 
 router
-    .post('/usuario', validaUsuario, UsuarioController.createUsuario);
+    .post('/usuario', auth, validaUsuario, UsuarioController.createUsuario);
 
 module.exports = router;
