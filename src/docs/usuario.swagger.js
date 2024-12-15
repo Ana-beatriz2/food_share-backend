dc62868d13b3e848fa525e216d27d278cab0d351
SarahElesbao <79275710+SarahElesbao@users.noreply.github.com>
2024-12-15 20:57:21 -0300

    adicionando retorno

--- src/docs/usuario.swagger.js
+++ src/docs/usuario.swagger.js
@@ -130,6 +130,36 @@
  *                   type: string
  *                   nullable: true
  *                   example: null
+ *             examples:
+ *               exemploDoador:
+ *                 value:
+ *                   id: "9d8af3b4-3941-4fab-a5a8-f11d460d048d"
+ *                   nome: "João Carlos 2"
+ *                   email: "joao2.silva@gmail.com"
+ *                   tipoUsuario: "doador"
+ *                   telefone: "1234567890"
+ *                   bairro: "Centro"
+ *                   cidade: "São Paulo"
+ *                   logradouro: "Rua Exemplo, 123"
+ *                   complemento: "Apto 101"
+ *                   estado: "SP"
+ *                   cpf: "12345678902"
+ *                   cnpj: null
+ *               exemploReceptor:
+ *                 value:
+ *                   id: "4090e555-1890-4f53-bb73-0e138a2b006e"
+ *                   nome: "Teste receptor"
+ *                   email: "testereceptor.silva@gmail.com"
+ *                   senha: "$2b$10$D2V3lxMES1Lnj5JsWVJeM.8Cdg5oUVYFBvLcLeT10SFMcU3xw.5g6"
+ *                   tipoUsuario: "receptor"
+ *                   telefone: "1234567890"
+ *                   bairro: "Centro"
+ *                   cidade: "São Paulo"
+ *                   logradouro: "Rua Exemplo, 123"
+ *                   complemento: "Apto 101"
+ *                   estado: "SP"
+ *                   cpf: "12345678906"
+ *                   cnpj: null
  *       404:
  *         description: Usuário não encontrado
  */
