1218bbd1c44137523d99a8cbab610a061ffe7164
SarahElesbao <79275710+SarahElesbao@users.noreply.github.com>
2024-12-15 21:03:23 -0300

    adicionando retorno e reparando erros

--- src/docs/usuario.swagger.js
+++ src/docs/usuario.swagger.js
@@ -63,17 +63,49 @@
  *               properties:
  *                 id:
  *                   type: string
- *                   example: "123e4567-e89b-12d3-a456-426614174000"
+ *                   example: "4090e555-1890-4f53-bb73-0e138a2b006e"
  *                 nome:
  *                   type: string
- *                   example: João Silva
+ *                   example: "Teste receptor"
+ *                 email:
+ *                   type: string
+ *                   example: "testereceptor.silva@gmail.com"
+ *                 tipoUsuario:
+ *                   type: string
+ *                   example: "receptor"
+ *                 telefone:
+ *                   type: string
+ *                   example: "1234567890"
+ *                 bairro:
+ *                   type: string
+ *                   example: "Centro"
+ *                 cidade:
+ *                   type: string
+ *                   example: "São Paulo"
+ *                 logradouro:
+ *                   type: string
+ *                   example: "Rua Exemplo, 123"
+ *                 complemento:
+ *                   type: string
+ *                   example: "Apto 101"
+ *                 estado:
+ *                   type: string
+ *                   example: "SP"
+ *                 cpf:
+ *                   type: string
+ *                   example: "12345678906"
+ *                 cnpj:
+ *                   type: string
+ *                   nullable: true
+ *                   example: null
  *       400:
  *         description: Dados inválidos
  */
 
+
 /**
  * @swagger
- * /usuario/{id}:
+ /usuario/{id}:
  *   get:
  *     summary: Retorna os detalhes de um usuário pelo ID
  *     tags: [Usuários]
@@ -130,39 +162,9 @@
  *                   type: string
  *                   nullable: true
  *                   example: null
- *             examples:
- *               exemploDoador:
- *                 value:
- *                   id: "9d8af3b4-3941-4fab-a5a8-f11d460d048d"
- *                   nome: "João Carlos 2"
- *                   email: "joao2.silva@gmail.com"
- *                   tipoUsuario: "doador"
- *                   telefone: "1234567890"
- *                   bairro: "Centro"
- *                   cidade: "São Paulo"
- *                   logradouro: "Rua Exemplo, 123"
- *                   complemento: "Apto 101"
- *                   estado: "SP"
- *                   cpf: "12345678902"
- *                   cnpj: null
- *               exemploReceptor:
- *                 value:
- *                   id: "4090e555-1890-4f53-bb73-0e138a2b006e"
- *                   nome: "Teste receptor"
- *                   email: "testereceptor.silva@gmail.com"
- *                   senha: "$2b$10$D2V3lxMES1Lnj5JsWVJeM.8Cdg5oUVYFBvLcLeT10SFMcU3xw.5g6"
- *                   tipoUsuario: "receptor"
- *                   telefone: "1234567890"
- *                   bairro: "Centro"
- *                   cidade: "São Paulo"
- *                   logradouro: "Rua Exemplo, 123"
- *                   complemento: "Apto 101"
- *                   estado: "SP"
- *                   cpf: "12345678906"
- *                   cnpj: null
  *       404:
  *         description: Usuário não encontrado
- */
+*/
 
 /**
  * @swagger
