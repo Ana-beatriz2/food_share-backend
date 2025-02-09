f5e032993c0393cd4c107da472ed4fd98cd5a5b8
Ana-beatriz2 <caldas.ana9@gmail.com>
2025-02-09 16:16:21 -0300

    feat: expõe pasta de imagens

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { swaggerUi, swaggerDocs } = require('./docs/swaggerConfig');
 const cors = require('cors');
 const routes = require('./routes');
+const path = require("path");
 const app = express();
 const port = 3000;
 
@@ -9,6 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api', routes);
+app.use("/uploads", express.static(path.join(__dirname, "public")));
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
